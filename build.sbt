3ac5fa7838770f7bc6a90c6d716070be21f197f5
Brandon Holt <bholt@cs.washington.edu>
2014-06-29 17:48:13 -0700

    working on getting finalge stream working

--- build.sbt
+++ build.sbt
@@ -2,7 +2,10 @@ name := "bennu"
 
 version := "0.1.0"
 
+resolvers += "twitter" at "http://maven.twttr.com"
+
 libraryDependencies ++= Seq(
   "com.twitter" %% "finagle-core" % "6.16.0",
-  "com.twitter" %% "finagle-http" % "6.16.0"
+  "com.twitter" %% "finagle-http" % "6.16.0",
+  "com.twitter" %% "finagle-stream" % "6.16.0"
 )
--- src/main/scala/Client.scala
+++ src/main/scala/Client.scala
@@ -1,41 +1,65 @@
 import com.twitter.finagle.builder.ClientBuilder
-import com.twitter.finagle.Service
-import com.twitter.finagle.http.Http
-import com.twitter.util.{Await, Future}
-import org.jboss.netty.handler.codec.http._
-import java.net.InetSocketAddress
+import com.twitter.finagle.ServiceFactory
+import com.twitter.finagle.stream.{Stream, StreamResponse}
+import com.twitter.conversions.time._
 import com.twitter.io.Charsets
+import com.twitter.util.{Base64StringEncoder => Base64, Future}
+import org.jboss.netty.handler.codec.http.{HttpRequest, HttpVersion, HttpMethod, DefaultHttpRequest}
+
 
 object Client {
 
   def main(args: Array[String]) {
+    // val (key,secret,token,tokenSecret,path) = args.slice(0, 5)
+    val (password, path) = (args(0), args(1))
 
-    val client: Service[HttpRequest, HttpResponse] = ClientBuilder()
-      .codec(Http())
-      .hosts(new InetSocketAddress(9090))
-      .hostConnectionLimit(1)
-      .build()
+    val clientFactory: ServiceFactory[HttpRequest, StreamResponse] =
+      ClientBuilder()
+        .codec(Stream())
+        .hosts("api.twitter.com:80")
+        .tcpConnectTimeout(1.microsecond)
+        .hostConnectionLimit(1)
+        .buildFactory()
 
-    val request1 = new DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, "/")
-    request1.headers().add("X-Request-ID", "12345")
-    val request2 = new DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, "/")
+    val request = new DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, path)
+    request.headers.set("Authorization", "Basic " +
+      Base64.encode("holtbg:$password".getBytes("UTF-8")))
+    request.headers.set("User-Agent", "Finagle 0.0")
+    request.headers.set("Host", "api.twitter.com:80")
 
-    val response1: Future[HttpResponse] = client(request1)
-    val response2: Future[HttpResponse] = client(request2)
+    println(request)
 
-    response1 onSuccess { resp =>
-      val responseString = resp.getContent.toString(Charsets.Utf8)
-      println("Response from server: " + responseString)
+    for { client <- clientFactory(); streamResponse <- client(request) } {
+      val httpResponse = streamResponse.httpResponse
+      if (httpResponse.getStatus.getCode != 200) {
+        println(httpResponse.toString)
+        client.close()
+        clientFactory.close()
+      } else {
+        var messageCount = 0 // Wait for 1000 messages then shut down.
+        streamResponse.messages foreach { buffer =>
+          messageCount += 1
+          println(buffer.toString(Charsets.Utf8))
+          println("--")
+          if (messageCount == 1000) {
+            client.close()
+            clientFactory.close()
+          }
+          // We return a Future indicating when we've completed processing the message.
+          Future.Done
+        }
+      }
     }
 
-    response2 onSuccess { resp =>
-      val responseString = resp.getContent.toString(Charsets.Utf8)
-      println("Response from server: " + responseString)
-    }
+//    val client = clientFactory()
+//    val stream = client(request)
+//
+//    stream onSuccess { resp =>
+//      resp.messages foreach { buff =>
+//        println(buff.toString(Charsets.Utf8))
+//      }
+//    }
 
-    (response1 join response2) ensure {
-      client.close()
-    }
   }
 
 }
